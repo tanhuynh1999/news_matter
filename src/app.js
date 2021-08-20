dcd6f557c249942560a16e7d9e3ef500d4f4f3b0
Tan Huynh <huynhminhtan4019@gmail.com>
2021-08-20 19:22:53 +0700

    test commit

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express();
 const port = 3000;
 
 //Morgan
-  app.use(morgan('combined'));
+app.use(morgan('combined'));
 
 //express-handlebars
 app.engine(
@@ -17,13 +17,13 @@ app.engine(
         extname: '.hbs',
     }),
 );
-              app.set('view engine', 'hbs');
+app.set('view engine', 'hbs');
 
-      app.set('views', path.join(__dirname, '/resources/views'));
+app.set('views', path.join(__dirname, '/resources/views'));
 
 //Routes init
 route(app);
 
-      app.listen(port, () => {
-          console.log(`Example app listening at http://localhost:${port}`);
-      });
+                  app.listen(port, () => {
+                      console.log(`Example app listening at http://localhost:${port}`);
+                  });
