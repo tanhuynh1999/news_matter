73239081ccd339d86ab69fd3a1c84dae983c69e7
Tan Huynh <huynhminhtan4019@gmail.com>
2021-08-20 19:13:34 +0700

    Install 3 husky - lint-staged - prettier libraries

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express();
 const port = 3000;
 
 //Morgan
-app.use(morgan('combined'));
+  app.use(morgan('combined'));
 
 //express-handlebars
 app.engine(
@@ -17,9 +17,9 @@ app.engine(
         extname: '.hbs',
     }),
 );
-app.set('view engine', 'hbs');
+              app.set('view engine', 'hbs');
 
-app.set('views', path.join(__dirname, '/resources/views'));
+      app.set('views', path.join(__dirname, '/resources/views'));
 
 //Routes init
 route(app);
